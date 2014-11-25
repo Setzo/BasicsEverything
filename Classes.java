f1bf32266e2f765d2b8e2c178037d24c53f9fcc7
Setzo <setzo94@gmail.com>
2014-11-25 16:13:18 +0100

    Create Classes.java

---
+++ Classes.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+
+class Person {
+	
+	private String nam;
+	private int age;
+	
+	public void setName(String newName) {
+		
+		nam = newName;
+	}
+	
+	public void setAge(int age) {
+		
+		this.age = age;
+	}
+	
+	void introduce() {
+		
+		System.out.println(nam + " | " + age);
+	}
+	
+	int yearsToRet() {								//oblicza lata do emerytury
+		
+		int y=65;
+		y=y-age;
+		return y;
+	}
+	
+	int getAge() {
+		
+		return age;
+	}
+	
+	public void setInfo(String name, int age) {
+		
+		setName(name);
+		setAge(age);
+	}
+}
+
+public class Classes {
+	public static void main(String[] args) {
+		
+		Person pr1 = new Person();
+		Scanner data = new Scanner(System.in);
+		pr1.setInfo(data.nextLine(),data.nextInt());
+		pr1.introduce();
+		
+		System.out.println(pr1.yearsToRet());
+		data.close();
+	}
+}
