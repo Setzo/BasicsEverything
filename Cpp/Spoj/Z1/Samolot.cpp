db8b7e362e71620fbbc6fbe51aa91186370bf938
Setzo <setzo94@gmail.com>
2014-11-25 17:35:56 +0100

    Create Samolot.cpp

---
+++ Cpp/Spoj/Z1/Samolot.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h> 
+#include <stdlib.h> 
+int main(int argc, char *argv[]) { 
+        int n1, n2, k1, k2, x=0; 
+        scanf("%d %d %d %d", &n1, &k1, &n2, &k2); 
+        wynik=(n1*k1)+(n2*k2); 
+        printf("%d", x); 
+        return 0; 
+}
