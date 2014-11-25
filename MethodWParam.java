3a1a79694fb3ec65871545b6c6052e077791cfe0
Setzo <setzo94@gmail.com>
2014-11-25 16:14:20 +0100

    Create MethodWParam.java

---
+++ MethodWParam.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class multiply {
+	
+	public int dod(int x, int y) {
+		return x+y;
+	}
+}
+
+public class MethWParam {
+	public static void main(String[] args) {
+		
+		
+		Scanner data = new Scanner(System.in);
+		multiply funkcja = new multiply();
+		System.out.println(funkcja.dod(data.nextInt(), data.nextInt()));
+		data.close();
+		
+	}
+}
