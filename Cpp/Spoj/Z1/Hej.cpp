5b6b2a7d4e56258157032eb89ca9779eee3a90ae
Setzo <setzo94@gmail.com>
2014-11-25 17:35:10 +0100

    Create Hej.cpp

---
+++ Cpp/Spoj/Z1/Hej.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+	int x;
+	cin >> x;
+	cout << "H";
+	for (int i= 0; i < x; i++)
+	{
+		cout << "e";
+	}
+	cout << "j";
+	return 0;
+}
