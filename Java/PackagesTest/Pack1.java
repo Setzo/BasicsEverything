e0c635c1721b5ffcc0298e6e174b8326de86df61
Setzo <setzo94@gmail.com>
2014-11-26 20:12:31 +0100

    Create Pack1.java

---
+++ Java/PackagesTest/Pack1.java
@@ -0,0 +1,17 @@
+package packa1;
+
+import packa1.packa3.Hello;
+import packa2.Pack2;
+
+public class Pack1 {
+	public static void main(String[] args) {
+	
+		Hello var1 = new Hello();					//packa1.packa3 public class Hello - odwołanie do klasy Hello, tworzenie nowe
+		
+		var1.sayHello();							//wywołanie metody sayHello z klasy Hello
+		
+		Pack2 var2 = new Pack2();					//packa2 public class Pack2
+													//nie sa się wywoływać metody welcome() z klasy Wel (packa2) class Wel
+		var2.sayWelcome2();							//wywoływanie metody sayWelcome2() z klasy Pack2
+	}
+}
