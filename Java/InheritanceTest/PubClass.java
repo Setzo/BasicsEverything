748e1fabccd74f8870d8b2431dfa167310ce73ac
Setzo <setzo94@gmail.com>
2014-11-26 16:04:37 +0100

    Create PubClass.java

---
+++ Java/InheritanceTest/PubClass.java
@@ -0,0 +1,18 @@
+
+public class PubClass {
+	
+ 	public String name = "Wojtek";
+	protected String name2 = "W2";
+	//private String name3 = "W3";
+	
+	public void f1() {
+		
+		System.out.println("f1 Wykonane (PubClass)");
+	}
+	
+	public void f2() {
+		
+		System.out.println("f2 wykonane (PubClass)");
+		
+	}
+}
