e92abd75d7482ede2eaaf9be216526515b1cfb6b
Setzo <setzo94@gmail.com>
2014-11-26 20:40:23 +0100

    Create Machine.java

---
+++ Java/InterfacesTest/Machine.java
@@ -0,0 +1,12 @@
+public class Machine implements IInfo {
+     
+    private int id = 7;
+     
+    public void start() {
+        System.out.println("Machine started.");
+    }
+ 
+    public void showInfo() {
+        System.out.println("Machine ID is: " + id);
+    }
+}
