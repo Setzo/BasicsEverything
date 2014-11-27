df2d3863d9f06a38f6e321fef43bcb8377b76583
Setzo <setzo94@gmail.com>
2014-11-27 20:03:50 +0100

    Create Peace.java

---
+++ Java/VisibilityTest/Peace.java
@@ -0,0 +1,13 @@
+import pack1.Plant;
+ 
+public class Peace {
+    public static void main(String[] args) {
+    	
+        Plant plant = new Plant();
+         
+        System.out.println(plant.name);
+         
+        System.out.println(plant.ID);
+     
+    }
+}
