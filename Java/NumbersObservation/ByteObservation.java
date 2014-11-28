a8e44518c342dcf65bd73e0de18f04d456e9b700
Setzo <setzo94@gmail.com>
2014-11-28 17:39:08 +0100

    Update ByteObservation.java

--- Java/NumbersObservation/ByteObservation.java
+++ Java/NumbersObservation/ByteObservation.java
@@ -2,7 +2,7 @@
 public class ByteObservation {
 	public static void main(String[] args) {
 
-		byte byteVal;
+		byte byteVal,cnt=0;
 
 		//byteVal = (byte) 128;
 		//System.out.println(byteVal);
@@ -11,13 +11,19 @@ public class ByteObservation {
 		//System.out.println(byteVal);
 		
 		for(int i=0;i<=999;i++) {
-			
-			byteVal=(byte)i;
-			System.out.printf("%3d %4d | ",i,byteVal);
 			if((i%50==0)&&(i!=0)) {
-				System.out.printf("\n\n");
+				System.out.printf("\n");
+				for(int j=0;j<=48;j++) {
+					if(j==0) {
+						System.out.printf("%10s|", " ");
+					}
+					System.out.printf("%11s|", " ");
+				}
+				System.out.printf("\n");
 			}
-		}
-		
+			byteVal=(byte)i;
+			System.out.printf("%4d %4d | ",i,byteVal);
+			
+		}	
 	}
 }
