1bbf0b63f63a211a60b852db7f9d70b7138181a6
Setzo <setzo94@gmail.com>
2014-11-29 14:53:40 +0100

    Create FileScannerTest.java

---
+++ Java/FileScannerTest.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+
+public class FileSc {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		String byteObs = "C:/Users/Setzo/Desktop/byteObs.txt";
+		File textFile = new File(byteObs);
+		Scanner input = new Scanner(textFile);
+		
+		while(input.hasNextLine()) {
+			String l = input.nextLine();
+			System.out.println(l);
+		}
+		
+		input.close();
+	}
+}
