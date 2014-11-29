d6fe28d8d21a06c6ba6fe39e91cdd1e0adc7cc56
Setzo <setzo94@gmail.com>
2014-11-29 15:22:42 +0100

    Create AbstractClass.java

---
+++ Java/AbstractClass/AbstractClass.java
@@ -0,0 +1,33 @@
+class Dod {
+	
+	public int dod(int a, int b) {
+		return a+b;
+	}
+
+	public String toString() {
+		return "Normal class";
+	}
+}
+
+
+public class AnonClass {
+	public static void main(String[] args) {
+		
+		Dod x = new Dod() {
+			@Override public int dod(int a,  int b) {
+				return a-b;
+			}
+			
+			@Override public String toString() {
+				return "Anonymous class injected";
+			}
+		};
+		Dod y = new Dod();
+		
+		System.out.println(x);
+		System.out.println(y);
+		
+		System.out.println(x.dod(5, 7));
+		System.out.println(y.dod(5, 7));
+	}
+}
