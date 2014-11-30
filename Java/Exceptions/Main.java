282a34c4a5dfbdf45b9d6316b0a010ea9cf39287
Setzo <setzo94@gmail.com>
2014-11-30 14:52:03 +0100

    Create Main.java

---
+++ Java/Exceptions/Main.java
@@ -0,0 +1,23 @@
+package demo1;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+ 
+ 
+public class App {
+ 
+    public static void main(String[] args) throws FileNotFoundException {
+         
+        File file = new File("byteObs.txt");
+         
+        FileReader fr = new FileReader(file);
+        try {
+			fr.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+ 
+}
