0250d42f2627592a70437e5a6622a38048f0a404
Setzo <setzo94@gmail.com>
2014-12-04 15:22:08 +0100

    Create Tab.java

---
+++ Java/FileWR/Tab.java
@@ -0,0 +1,55 @@
+public class Tab {
+
+	Tab() {
+
+		ID = cnt;
+		cnt++;
+	}
+
+	private int ID = 0;
+	private static int cnt = 0;
+	private short[] tab = new short[10000];
+
+	public int getID() {
+		return ID;
+	}
+
+	public static int getCnt() {
+		return cnt;
+	}
+
+	public short[] getTab() {
+		return tab;
+	}
+
+	public short getTabElement(int i) {
+		return tab[i];
+	}
+
+	public void setTabElement(int i, short x) {
+		tab[i] = x;
+	}
+
+	public void setTab(short[] tab) {
+		this.tab = tab;
+	}
+	
+	public void writeTabElement(int i) {
+		System.out.print(tab[i]);
+	}
+	
+	public boolean hasNxt(int i) {
+		if(tab[i+1]>=-128 && tab[i+1]<=127) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	public void writeTab() {
+		for(int i=0; i<10000; i++) {
+			System.out.printf("Value : %6d",tab[i]);
+		}
+	}
+}
