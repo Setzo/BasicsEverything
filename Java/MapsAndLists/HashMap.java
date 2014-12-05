51523e78e2fee63248a35c6c35da4b1c02b0c0d6
Setzo <setzo94@gmail.com>
2014-12-05 14:15:45 +0100

    Create HashMap.java

---
+++ Java/MapsAndLists/HashMap.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class HM {
+    public static void main(String[] args) {
+
+        HashMap<Integer, String> map = new HashMap<Integer, String>();
+        
+        map.put(5, "Five");
+        map.put(8, "Eight");
+        map.put(6, "Six");
+        map.put(4, "Four");
+        map.put(2, "Two");
+        
+        System.out.println(map.get(4));
+        
+        for(Map.Entry<Integer, String> entry: map.entrySet()) {
+        	
+            int key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + ": " + value);
+        }
+    }
+}
