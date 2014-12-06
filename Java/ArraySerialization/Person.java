97561d0eb54bead779e2264f2105fc030a65d45d
Setzo <setzo94@gmail.com>
2014-12-06 18:27:54 +0100

    Create Person.java

---
+++ Java/ArraySerialization/Person.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+
+public class Person implements Serializable {
+
+	private static final long serialVersionUID = -4103124626927806588L;
+	private static int cnt=0;
+	private int id;
+	private String name;
+	
+	public Person(String name) {
+		id=cnt;
+		cnt++;
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Person #" + id + ", is named: " + name;
+	}
+
+}
