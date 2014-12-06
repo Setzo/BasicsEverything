227984f0567e53ab480cb3d08be98d6e778ce284
Setzo <setzo94@gmail.com>
2014-12-06 22:12:36 +0100

    Create HelloWorld.java

---
+++ Java.Swing/Beginning/HelloWorld.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+
+public class Application {
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(new Runnable(){
+
+			@Override
+			public void run() {
+				JFrame helloWorld = new JFrame("Hello World! :D");
+				helloWorld.setVisible(true);
+				helloWorld.setSize(500, 500);
+				helloWorld.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);			//JFrame.EXIT_ON_CLOSE = 3
+			}
+			});
+	}
+}
