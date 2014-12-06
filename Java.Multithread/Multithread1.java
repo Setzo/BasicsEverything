b59d327a65e51448c84de4d9f5b9fa03ac67edac
Setzo <setzo94@gmail.com>
2014-12-06 23:14:08 +0100

    Create Multithread1.java

---
+++ Java.Multithread/Multithread1.java
@@ -0,0 +1,28 @@
+package package1;
+
+class Runner extends Thread {
+
+	@Override
+	public void run() {
+		for(int i=0; i<1000; i++) {
+			System.out.println("Hello: " + i);
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				//e.printStackTrace();
+			}
+		}
+	}
+}
+
+public class Multithread {
+	public static void main(String[] args) {
+		
+		Runner r1 = new Runner();
+		r1.start();
+		
+		Runner r2 = new Runner();
+		r2.start();
+		
+	}
+}
