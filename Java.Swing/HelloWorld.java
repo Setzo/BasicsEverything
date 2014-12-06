11275f72a9e940d612f7ea48869b9da7e95e1433
Setzo <setzo94@gmail.com>
2014-12-06 23:20:03 +0100

    Update HelloWorld.java

--- Java.Swing/HelloWorld.java
+++ Java.Swing/HelloWorld.java
@@ -6,7 +6,7 @@ public class Application {
 	public static void main(String[] args) {
 		
 		SwingUtilities.invokeLater(new Runnable(){
-
+ 
 			@Override
 			public void run() {
 				JFrame helloWorld = new JFrame("Hello World! :D");
