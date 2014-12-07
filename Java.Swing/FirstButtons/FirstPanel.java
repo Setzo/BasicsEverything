5e761022435dec499ee047733b023411c00f8dbe
Setzo <setzo94@gmail.com>
2014-12-07 22:09:54 +0100

    Create FirstPanel.java

---
+++ Java.Swing/FirstButtons/FirstPanel.java
@@ -0,0 +1,24 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+
+
+public class TextPanel extends JPanel {
+	
+	private static final long serialVersionUID = 4933762970216328125L;
+	
+	private JTextArea textArea ;
+	TextPanel() {
+		textArea = new JTextArea();
+		
+		setLayout(new BorderLayout());
+		
+		add(new JScrollPane(textArea), BorderLayout.CENTER);
+	}
+	
+	public void appendText(String c) {
+		textArea.append(c);
+	}
+}
