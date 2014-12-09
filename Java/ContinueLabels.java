c18e4555be962d2998b01962671e0fab7bff5d52
Setzo <setzo94@gmail.com>
2014-12-09 22:45:53 +0100

    Create ContinueLabels.java

---
+++ Java/ContinueLabels.java
@@ -0,0 +1,11 @@
+public class Labels {
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		loop: 
+			for (int i = 0; i <= 2; i++) {
+				System.out.println(" ddsd");
+				if(true) {continue loop;}
+				System.out.println("eee");
+			}
+	}
+}
