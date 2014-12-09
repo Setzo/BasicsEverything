e55b30556a21c43c1008371dfb691156b18cb92a
Setzo <setzo94@gmail.com>
2014-12-09 15:19:03 +0100

    Update 10.java

--- Java.Euler/10.java
+++ Java.Euler/10.java
@@ -5,14 +5,16 @@ public class Euler10 {
 	public static void main(String[] args) {
 		
 		long sum=2;
-		for(int i=2; i<2000000; i++) {
-			for(int j=2; j<i; j++) {
-				if(i%j==0) {
-					break;
-				}
-				if(j==i-1) {
-					sum=sum+i;
-				}
+		int n=2000000;
+		for(int i=3; i<n; i+=2) {
+			loop:
+			{
+				for(int j=3; j<=Math.sqrt(i); j+=2) {
+					if(i%j==0) {
+						break loop;
+					}
+			}
+			sum += i;
 			}
 		}
 		System.out.println(sum);
