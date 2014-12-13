2ea884ef94367a809f9d6029b6cd9b9c2af57865
Setzo <setzo94@gmail.com>
2014-12-13 19:11:28 +0100

    Update 28.java

--- Java.Euler/28.java
+++ Java.Euler/28.java
@@ -2,8 +2,7 @@ package euler28;
 
 public class Euler28 {
 	public static void main(String[] args) {
-		long sum=1;
-		Long lastNum = (long)1;
+		long sum=1, lastNum=1;
 		for(int i=3; i<=1001; i+=2) {
 			sum = sum +(lastNum*4)+(10*(i-1));
 			lastNum = lastNum + 4*(i-1);
