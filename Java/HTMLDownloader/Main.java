508602ff77e59ea472ecfd23f69478c9640bde05
Setzo <setzo94@gmail.com>
2014-12-21 20:48:53 +0100

    Create Main.java

---
+++ Java/HTMLDownloader/Main.java
@@ -0,0 +1,12 @@
+package iterab;
+
+public class UsingIterable {
+	public static void main(String[] args) {
+		UrlLib ul = new UrlLib();
+		
+		for (String html : ul) {
+			System.out.println(html);
+			System.out.println(html.length());
+		}
+	}
+}
