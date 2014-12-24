389cf835262daeb5abaeb51237b6ca3584daaf91
Setzo <setzo94@gmail.com>
2014-12-24 15:17:40 +0100

    Create Processor.java

---
+++ Java.Multithread/LowLvlSynchronization/Processor.java
@@ -0,0 +1,43 @@
+package lowLvlSynchro;
+
+import java.util.LinkedList;
+import java.util.Random;
+
+public class Processor {
+	
+	private LinkedList<Integer> list = new LinkedList<Integer>();
+	private final int LIMIT = 10;
+	private Object lock = new Object();
+	
+	public void produce() throws InterruptedException {
+		
+		int val = 0;
+		while(true) {
+			synchronized (lock) {
+				while(list.size()==LIMIT) {
+					lock.wait();
+				}
+				list.add(val++);
+				lock.notify();
+			}
+		}
+	}
+	
+	public void consume() throws InterruptedException {
+		
+		Random rng = new Random();
+		while(true) {
+			synchronized (lock) {
+				
+				while(list.size()==0) {
+					lock.wait();
+				}
+				System.out.print(list.size());
+				int val = list.removeFirst();
+				System.out.println(" " + val);
+				lock.notify();
+			}
+			Thread.sleep(rng.nextInt(1000));
+		}
+	}
+}
