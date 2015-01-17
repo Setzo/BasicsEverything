c0ea3bc9dc28a1adafdb33b219d239b78c553873
Setzo <setzo94@gmail.com>
2015-01-17 20:17:40 +0100

    Create Wiatraczki.cpp

---
+++ Cpp/Spoj/Z9/Wiatraczki.cpp
@@ -0,0 +1,85 @@
+#include <algorithm>
+
+int main() {
+
+	while (true) {
+
+		int n;
+		bool negate = true;
+
+		scanf_s("%d", &n);
+
+		if (n == 0) {
+			break;
+		}
+
+		if (n < 0) {
+			n = abs(n);
+			negate = false;
+		}
+
+		char tab[400][400];
+
+		for (int i = 0; i < 2 * n; i++) {
+			for (int j = 0; j < 2 * n; j++) {
+
+				tab[i][j] = '.';
+			}
+		}
+
+		int j = 0;
+		for (int i = 0; i < n; i++) {
+
+			//PRAWY DOLNY - IF NEGATE
+			for (int x = 1; x + j < n; x++) {
+				tab[n + j][n + j + x] = '*';
+			}
+
+			//LEWY DOLNY - IF NEGATE
+			for (int x = 1; x + j < n; x++) {
+				tab[n + j + x][n - j - 1] = '*';
+			}
+
+			//LEWY GORNY - IF NEGATE
+			for (int x = 1; x + j < n; x++) {
+				tab[n - j - 1][n - j - 1 - x] = '*';
+			}
+
+			//PRAWY GORNY - IF NEGATE
+			for (int x = 1; x + j < n; x++) {
+				tab[n - j - 1 - x][n + j] = '*';
+			}
+
+			//POPRZECZNE
+			tab[n + j][n - j - 1] = '*';
+			tab[n + j][n + j] = '*';
+			tab[n - j - 1][n - j - 1] = '*';
+			tab[n - j - 1][n + j] = '*';
+
+			j++;
+		}
+
+		if (negate) {
+
+			for (int i = 0; i < 2 * n; i++) {
+				for (int j = 0; j < 2 * n; j++) {
+
+					printf("%c", tab[i][j]);
+				}
+				printf("\n");
+			}
+		}
+		else {
+
+			for (int i = 0; i < 2 * n; i++) {
+				for (int j = 0; j < 2 * n; j++) {
+
+					printf("%c", tab[j][i]);
+				}
+				printf("\n");
+			}
+		}
+	}
+
+	return 0;
+}
