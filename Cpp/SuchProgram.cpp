83b27e405c2f66bc54b2777a51e4968fb8116f41
Wojtek Pruszak <setzo94@gmail.com>
2015-01-28 13:19:43 +0100

    Create SuchProgram.cpp

---
+++ Cpp/SuchProgram.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+
+#define such int
+#define much bool
+#define code bo;
+#define so printf
+#define wow ("such int\nmuch doge\nso printf\n\n");
+#define fucksgiven return
+#define very system
+#define stahp ("pause");
+#define I ;
+#define program main(){
+#define zero 0;}
+
+such program
+
+much code
+so wow
+very stahp
+
+fucksgiven zero
