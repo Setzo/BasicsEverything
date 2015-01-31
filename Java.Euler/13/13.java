e0c19aba7b93f329f1e755234920762a5b9446e0
Wojtek Pruszak <setzo94@gmail.com>
2015-02-01 00:31:15 +0100

    Create 13.java

---
+++ Java.Euler/13/13.java
@@ -0,0 +1,24 @@
+package euler13;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Euler13 {
+	public static void main(String[] args) {
+		
+		BigInteger sum = BigInteger.ZERO;
+		BigInteger x = null;
+		
+		try(Scanner data = new Scanner(Euler13.class.getResourceAsStream("euler13.txt"))) {
+			
+			while(data.hasNextLine()) {
+				x = new BigInteger(data.nextLine());
+				sum = sum.add(x);
+			}
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(sum.toString().substring(0, 10));
+	}
+}
