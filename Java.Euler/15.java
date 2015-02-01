cda9ee6462b17cfcc8e9b9046d1d90c9dcf80e19
Wojtek Pruszak <setzo94@gmail.com>
2015-02-01 11:41:17 +0100

    Create 15.java

---
+++ Java.Euler/15.java
@@ -0,0 +1,21 @@
+package euler15;
+
+public class Euler15 {
+
+	public static void main(String[] args) {
+
+		System.out.print(count(40, 20));
+	}
+
+	public static long count(int movement, int gridDimension) {
+
+		long newtonSymbol = 1;
+
+		for (int i = 0; i < gridDimension; i++) {
+			
+			newtonSymbol *= (movement - i);
+			newtonSymbol /= (i+1); 
+		}
+		return newtonSymbol;
+	}
+}
