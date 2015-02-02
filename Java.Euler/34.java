e5461c69619c7bf8f1e17000d1ff19d60f400175
Wojtek Pruszak <setzo94@gmail.com>
2015-02-02 14:56:28 +0100

    Create 34.java

---
+++ Java.Euler/34.java
@@ -0,0 +1,48 @@
+package euler34;
+
+
+public class Euler34 {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		int a = 0;
+		
+		countFSum(40585);
+		
+		for (long i = 3; i < 10000000; i++) {
+			
+			if(countFSum(i) == i) {
+				a += i;
+			}
+		}
+		
+		System.out.print(a);
+		
+	}
+
+	public static long countF(long a) {
+
+		if (a < 0) {
+			return -1;
+		}
+		if (a == 0) {
+			return 1;
+		}
+		if (a == 1) {
+			return a;
+		}
+		return countF(a - 1) * a;
+	}
+	
+	public static long countFSum(long a) {
+		
+		long sum = 0;
+		
+		while (a != 0) {
+			sum += countF(a % 10);
+			a /= 10;
+		}
+		
+		return sum;
+	}
+}
