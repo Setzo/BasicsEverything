8ed27f858d79025583b3267a66bfa9f53377b46b
Wojtek Pruszak <setzo94@gmail.com>
2015-02-07 16:08:38 +0100

    Create 80.java

---
+++ Java.Euler/80.java
@@ -0,0 +1,51 @@
+package euler80;
+
+import java.math.BigDecimal;
+
+public class Euler80 {
+
+	public static void main(String[] args) {
+		
+		int sigma = 0;
+		
+		for(int i = 1; i < 100; i++) {
+			if(Math.sqrt(i) % 1 != 0) {
+				sigma += digitSigma(getSubstring(getValue(new BigDecimal(i))));
+			}
+		}
+		
+		System.out.println(sigma);
+	}
+	
+	public static int digitSigma(String substr) {
+		
+		int sum = 0;
+		
+		for(int i = 0; i < substr.length(); i++) {
+			sum += Character.getNumericValue(substr.charAt(i));
+		}
+		
+		return sum;
+	}
+	
+	public static String getSubstring(BigDecimal num) {
+		
+		return num.toString().substring(0, 101).replace(".", "");
+	}
+	
+	public static BigDecimal getValue(BigDecimal num) {
+		
+		BigDecimal sqrtNum = new BigDecimal(Math.sqrt(num.doubleValue()));
+		BigDecimal divider = new BigDecimal("0");
+		
+		while(!divider.equals(sqrtNum)) {
+			
+			divider = sqrtNum;
+			sqrtNum = num.divide(divider, 150, BigDecimal.ROUND_HALF_DOWN);
+			sqrtNum = sqrtNum.add(divider);
+			sqrtNum = sqrtNum.divide(new BigDecimal("2"), 150, BigDecimal.ROUND_HALF_DOWN);
+		}
+		
+		return sqrtNum;
+	}
+}
