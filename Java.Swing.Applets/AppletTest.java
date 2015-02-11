4bd640a85d99590f3c04f93662d01534af55f8c2
Wojtek Pruszak <setzo94@gmail.com>
2015-02-11 20:14:12 +0100

    Create AppletTest.java

---
+++ Java.Swing.Applets/AppletTest.java
@@ -0,0 +1,30 @@
+import java.awt.BorderLayout;
+
+import javax.swing.JApplet;
+
+
+public class AppletTest extends JApplet {
+
+	private static final long serialVersionUID = -5528671500902698492L;
+
+	public void destroy() {
+		System.out.println("Destroy");
+	}
+
+	public void init() {
+		
+		setSize(750, 750);
+		setLayout(new BorderLayout());
+		add(new Game(), BorderLayout.CENTER);
+		
+		System.out.println("Init");
+	}
+
+	public void start() {
+		System.out.println("Start");
+	}
+
+	public void stop() {
+		System.out.println("Stop");
+	}
+}
