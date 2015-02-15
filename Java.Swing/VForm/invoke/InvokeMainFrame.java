12f4cfc6893174f100e28f25db1c431d4af87048
Wojtek Pruszak <setzo94@gmail.com>
2015-02-15 20:48:55 +0100

    Update InvokeMainFrame.java

--- Java.Swing/VForm/invoke/InvokeMainFrame.java
+++ Java.Swing/VForm/invoke/InvokeMainFrame.java
@@ -8,12 +8,6 @@ import javax.swing.SwingUtilities;
 public class InvokeMainFrame {
 	public static void main(String[] args) {
 		
-		SwingUtilities.invokeLater(new Runnable(){
-
-			@Override
-			public void run() {
-				new MainFrame("VForm alpha 0.4.4");
-			}
-		});
+		SwingUtilities.invokeLater(() -> new MainFrame("VForm"));
 	}
 }
