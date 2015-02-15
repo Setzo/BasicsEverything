5209b7a90ad5bea848a821a80fb0c43bdba433f5
Wojtek Pruszak <setzo94@gmail.com>
2015-02-15 20:45:19 +0100

    Update MessagePanel.java

--- Java.Swing/VForm/gui/MessagePanel.java
+++ Java.Swing/VForm/gui/MessagePanel.java
@@ -1,162 +1,221 @@
 package gui;
 
+import gui.listeners.ProgressDialogListener;
+
+import java.awt.BorderLayout;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.concurrent.ExecutionException;
 
-import javax.swing.table.AbstractTableModel;
+import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.JTree;
+import javax.swing.SwingWorker;
+import javax.swing.event.CellEditorListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.TreeSelectionModel;
 
-import data.AgeCategoryEnumerated;
-import data.EmploymentCategory;
-import data.GenderCategory;
-import data.Person;
+import controller.StimulatedMessageServer;
+import data.Message;
 
-public class PersonTableModel extends AbstractTableModel {
-
-	private static final long serialVersionUID = -644898091938774026L;
-
-	private List<Person> data;
+public class MessagePanel extends JPanel implements ProgressDialogListener {
 	
-	private String[] colNames = {"ID", "Name", "Occupation", "Age Category", "Employment", "Polish Citizen", "Document ID", "Gender", "Phone Number"};
+	private static final long serialVersionUID = -4998576709728752117L;
 	
-	public PersonTableModel(){
+	private JTree serverTree;
+	private ServerTreeCellRenderer treeRenderer;
+	private ServerTreeCellEditor treeEditor;
+	
+	private Set<Integer> selectedServers;
+	private StimulatedMessageServer mServer;
+	
+	private SwingWorker<List<Message>, Integer> sw;
+	
+	private ProgressDialog progressDialog;
+	private TextPanel textPanel;
+	private JList<Message> msgList;
+	private JSplitPane upperPane;
+	private JSplitPane lowerPane;
+	
+	private DefaultListModel<Message> listModel;
+	private MessageListRenderer msgListRenderer;
+	
+	protected MessagePanel(JFrame parent) {
+
+		listModel = new DefaultListModel<Message>();
+		progressDialog = new ProgressDialog(parent, "Downloading Messages...");
+		progressDialog.setProgressDialogListener(this);
+		
+		treeRenderer = new ServerTreeCellRenderer();
+		treeEditor = new ServerTreeCellEditor();
+		
+		selectedServers = new TreeSet<Integer>();
+		mServer = new StimulatedMessageServer();
+		
+		serverTree = new JTree(createTree());
+		serverTree.setCellRenderer(treeRenderer);
+		serverTree.setCellEditor(treeEditor);
+		serverTree.setEditable(true);
+		
+		serverTree.getSelectionModel().setSelectionMode(TreeSelectionModel.SINGLE_TREE_SELECTION);
+		
+		msgListRenderer = new MessageListRenderer();
+		
+		mServer.setSelectedServers(selectedServers);
+		
+		treeEditor.addCellEditorListener(new CellEditorListener() {
+			
+			@Override
+			public void editingStopped(ChangeEvent e) {
+				
+				ServerInfo info = (ServerInfo)treeEditor.getCellEditorValue();
+				
+				if(info.isChecked()) {
+					selectedServers.add(info.getId());
+				}
+				else if(!info.isChecked()) {
+					selectedServers.remove(info.getId());
+				}
+				
+				mServer.setSelectedServers(selectedServers);
+				
+				retrieveMessages();
+			}
+			
+			@Override
+			public void editingCanceled(ChangeEvent e) {
+			}
+		});
+		
+		setLayout(new BorderLayout());
+		
+		textPanel = new TextPanel();
+		msgList = new JList<Message>(listModel);
+		msgList.setCellRenderer(msgListRenderer);
+		
+		msgList.addListSelectionListener((e) -> {
+			
+			Message msg = msgList.getSelectedValue();
+
+			if(msg != null) {
+				textPanel.setText(String.format("Title: %s\n\n%s",
+						msg.getTitle(), 
+						msg.getText()));
+			}
+		});
+		
+		lowerPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new JScrollPane(msgList), textPanel);
+		upperPane = new JSplitPane(JSplitPane.VERTICAL_SPLIT, new JScrollPane(serverTree), lowerPane);
+		
+		//textPanel.setMinimumSize(new Dimension(10, 70));
+		//msgList.setMinimumSize(new Dimension(10, 70));
+		
+		upperPane.setResizeWeight(0.3);
+		lowerPane.setResizeWeight(0.5);
+		
+		lowerPane.setBorder(null);
+		upperPane.setBorder(null);
+		
+		add(upperPane, BorderLayout.CENTER);
 	}
 	
-	public boolean isCellEditable(int row, int col) {
+	private void retrieveMessages() {
 		
-		if(col == 0) {
-			return false;
-		} else if (col == 6 && (!((Person)data.get(row)).isPLCitizen())) {
-			return false;
-		} else {
-			return true;
-		}
+		progressDialog.setMaximum(mServer.getMessageCount());
+		
+		progressDialog.setVisible(true);
+		
+		sw = new SwingWorker<List<Message>, Integer>() {
+
+			protected List<Message> doInBackground() throws Exception {
+				
+				int cnt = 0;
+				
+				List<Message> retrievedMessageList = new LinkedList<Message>();
+				
+				for(Message msg : mServer) {
+					
+					if(isCancelled()) {
+						break;
+					}
+					
+					//textPanel.appendText(msg.toString());
+					retrievedMessageList.add(msg);
+					cnt++;
+					publish(cnt);
+				}
+				
+				return retrievedMessageList;
+			}
+
+			protected void done() {
+				
+				progressDialog.setVisible(false);
+				
+				if(isCancelled()) {
+					return;
+				}
+				
+				try {
+					
+					List<Message> retrievedMessageList = get();
+					listModel.removeAllElements();
+					
+					for(Message msg : retrievedMessageList) {
+						listModel.addElement(msg);
+					}
+					
+					msgList.setSelectedIndex(0);
+					
+				} catch (InterruptedException e) {
+				} catch (ExecutionException e) {
+				}
+			}
+
+			protected void process(List<Integer> msgCount) {
+				
+				progressDialog.setValue(msgCount.get(msgCount.size() - 1));
+			}
+		};
+		
+		sw.execute();
+	}
+	
+	private DefaultMutableTreeNode createTree() {
+		
+		DefaultMutableTreeNode top = new DefaultMutableTreeNode("Servers");
+		
+		DefaultMutableTreeNode branch0 = new DefaultMutableTreeNode("Poland");
+		
+		branch0.add(new DefaultMutableTreeNode(new ServerInfo(0, "Reda", selectedServers.contains(0))));
+		branch0.add(new DefaultMutableTreeNode(new ServerInfo(1, "Gdynia", selectedServers.contains(1))));
+		branch0.add(new DefaultMutableTreeNode(new ServerInfo(2, "Gdansk", selectedServers.contains(2))));
+		
+		DefaultMutableTreeNode branch1 = new DefaultMutableTreeNode("UK");
+		
+		branch1.add(new DefaultMutableTreeNode(new ServerInfo(3, "London", selectedServers.contains(3))));
+		branch1.add(new DefaultMutableTreeNode(new ServerInfo(4, "Edinburgh", selectedServers.contains(4))));
+		
+		top.add(branch0);
+		top.add(branch1);
+		
+		return top;
 	}
 
-	public void setValueAt(Object value, int row, int col) {
-		
-		if(data == null) {
-			return;
-		}
-		
-		switch(col) {
-		case 0 : {
-			return;
-		} case 1 : {
-			data.get(row).setName((String)value);
-			return;
-		} case 2 : {
-			data.get(row).setOccupation((String)value);
-			return;
-		} case 3 : {
-			data.get(row).setAge((AgeCategoryEnumerated)value);
-			return;
-		} case 4 : {
-			data.get(row).setEmployment((EmploymentCategory)value);
-			return;
-		} case 5 : {
-			data.get(row).setPLCitizen((boolean)value);
-			return;
-		} case 6 : {
-			data.get(row).setDocID((String)value);
-			return;
-		} case 7 : {
-			data.get(row).setGender((GenderCategory)value);
-			return;
-		} case 8 : {
-			data.get(row).setPhoneNumber((String)value);
-			return;
-		} default : {
-			return;
-		}
+	public void cancelButtonPressed() {
+		if(sw != null) {
+			sw.cancel(true);
 		}
 	}
 	
-	public Class<?> getColumnClass(int col) {
-		
-		switch(col) {
-		case 0: {
-			return Integer.class;
-		}
-		case 1: {
-			return String.class;
-		}
-		case 2: {
-			return String.class;
-		}
-		case 3: {
-			return AgeCategoryEnumerated.class;
-		}
-		case 4: {
-			return EmploymentCategory.class;
-		}
-		case 5: {
-			return Boolean.class;
-		}
-		case 6: {
-			return String.class;
-		}
-		case 7: {
-			return GenderCategory.class;
-		}
-		case 8: {
-			return String.class;
-		}
-		default : {
-			return null;
-		}
-		}
+	public void refresh() {
+		retrieveMessages();
 	}
-
-	public void setData(List<Person> data) {
-		this.data = data;
-	}
-	
-	public int getColumnCount() {
-		return 9;
-	}
-
-	public int getRowCount() {
-		return data.size();
-	}
-
-	public String getColumnName(int column) {
-		return colNames[column];
-	}
-
-	public Object getValueAt(int row, int col) {
-
-		Person person = data.get(row);
-		
-		switch(col) {
-		case 0: {
-			return person.getId();
-		}
-		case 1: {
-			return person.getName();
-		}
-		case 2: {
-			return person.getOccupation();
-		}
-		case 3: {
-			return person.getAge();
-		}
-		case 4: {
-			return person.getEmployment();
-		}
-		case 5: {
-			return person.isPLCitizen();
-		}
-		case 6: {
-			return person.getDocID();
-		}
-		case 7: {
-			return person.getGender();
-		}
-		case 8: {
-			return person.getPhoneNumber();
-		}
-		}
-		
-		return null;
-	}
-
 }
