f293b5839b5e0700c666a0fc07655a214fcc3f35
Wojtek Pruszak <setzo94@gmail.com>
2015-02-19 15:23:04 +0100

    Create Test.java

---
+++ Java.Servlets/ValidationTest/Test.java
@@ -0,0 +1,18 @@
+package temp;
+
+import beans.User;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		User user = new User("set@set.com", "Password");
+		
+		if(user.isValid()) {
+			System.out.println("Bean validated");
+		} else {
+			System.out.println(user.getValidationErrorMessage());
+		}
+
+	}
+}
