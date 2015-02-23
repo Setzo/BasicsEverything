d4f9aa9cc479b7ec5902188048660b32556c4ccc
Wojtek Pruszak <setzo94@gmail.com>
2015-02-23 22:23:37 +0100

    Update 10.java

--- Java.Euler/10.java
+++ Java.Euler/10.java
@@ -4,13 +4,13 @@ package euler10;
 public class Euler10 {
 	public static void main(String[] args) {
 		
-		long sum=2;
-		int n=2000000;
-		for(int i=3; i<n; i+=2) {
+		long sum = 2;
+		int n = 2000000;
+		for(int i = 3; i < n; i += 2) {
 			loop:
 			{
-				for(int j=3; j<=Math.sqrt(i); j+=2) {
-					if(i%j==0) {
+				for(int j = 3; j <= Math.sqrt(i); j += 2) {
+					if(i % j == 0) {
 						break loop;
 					}
 			}
