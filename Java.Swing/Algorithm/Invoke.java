eeeda25e457d8a984f7a803a83788422b21bb7c7
Wojtek Pruszak <setzo94@gmail.com>
2015-02-27 20:44:54 +0100

    Update and rename Java.Swing to Java.Swing/Algorithm/Invoke.java

---
+++ Java.Swing/Algorithm/Invoke.java
@@ -0,0 +1,15 @@
+package invoke;
+
+import javax.swing.SwingUtilities;
+
+import view.MainFrame;
+
+
+public class Invoke {
+
+	public static void main(String[] args) {
+		
+		SwingUtilities.invokeLater(() -> new MainFrame("Algorithms"));
+
+	}
+}
