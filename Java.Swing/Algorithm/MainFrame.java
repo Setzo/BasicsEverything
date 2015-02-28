9710b6bbdfa0b84cdb76d1f7920c31223870374e
Wojtek Pruszak <setzo94@gmail.com>
2015-02-28 19:13:52 +0100

    Update MainFrame.java

--- Java.Swing/Algorithm/MainFrame.java
+++ Java.Swing/Algorithm/MainFrame.java
@@ -1,11 +1,19 @@
 package view;
-import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
+import javax.swing.KeyStroke;
 import javax.swing.UIManager;
 import javax.swing.UIManager.LookAndFeelInfo;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -20,17 +28,24 @@ public class MainFrame extends JFrame {
 	
 	public MainFrame(String title) {
 		
-		setLookAndFeel();
-		instantiate();
+		//setLookAndFeel();
 		setLayoutStuff();
 		setParams(title);
+		
+		addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				
+				dispose();
+				System.gc();
+			}
+		});
 	}
 	
 	private void instantiate() {
 		
 		displayPanel = new DisplayPanel();
 		toolbar = new Toolbar();
-	}
+	}	
 	
 	private void setLookAndFeel() {
 		
@@ -57,7 +72,7 @@ public class MainFrame extends JFrame {
 	private void setParams(String title) {
 		
 		setTitle(title);
-		setSize(new Dimension(500, 500));
+		setSize(new Dimension(1000, 500));
 		setFocusable(true);
 		setResizable(true);
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -68,14 +83,35 @@ public class MainFrame extends JFrame {
 
 	private void setLayoutStuff() {
 		
-		setLayout(new BorderLayout());
+		instantiate();
 		
-		add(displayPanel, BorderLayout.CENTER);
-		add(toolbar, BorderLayout.NORTH);
+		setLayout(new GridBagLayout());
+		GridBagConstraints gc = new GridBagConstraints();
+		
+		int col = 0, row = 0;;
+		
+		////////////////ROW 0
+		gc.weightx = 0.1;
+		gc.weighty = 0.1;
+		gc.fill = GridBagConstraints.BOTH;
+		
+		gc.gridx = col;
+		gc.gridy = row;
+		
+		add(toolbar, gc);
+		
+		////////////////ROW++
+		gc.weightx = 4;
+		gc.weighty = 4;
+		
+		gc.gridx = col;
+		gc.gridy = ++row;
+		add(displayPanel, gc);
 	}
 	
 	private JMenuBar createMenuBar() {//TODO Make it work
 		
+		//INSTANTIATING
 		JMenuBar menuBar = new JMenuBar();
 		
 		JMenu fileMenu = new JMenu("File");
@@ -83,6 +119,32 @@ public class MainFrame extends JFrame {
 		
 		JMenuItem exitItem = new JMenuItem("Exit");
 		
+		//ADDING LISTENERS
+		exitItem.addActionListener((e) -> {
+			
+			
+			int action = JOptionPane.showConfirmDialog(MainFrame.this,
+					"Do you wish to exit?",
+					"Confirm Exit",
+					JOptionPane.OK_CANCEL_OPTION,
+					JOptionPane.WARNING_MESSAGE);
+			
+			if(action == JOptionPane.OK_OPTION) {
+				for(WindowListener listener : getWindowListeners()) {
+					listener.windowClosing(new WindowEvent(MainFrame.this, 0));
+				}
+			}
+		});
+		
+		//ADDING MNEMONICS & ACCELERATORS
+		fileMenu.setMnemonic(KeyEvent.VK_F);
+		windowMenu.setMnemonic(KeyEvent.VK_W);
+		
+		exitItem.setMnemonic(KeyEvent.VK_W);
+		
+		exitItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_W, ActionEvent.CTRL_MASK));
+		
+		//ADDING TO LAYOUT
 		fileMenu.add(exitItem);
 		
 		menuBar.add(fileMenu);
