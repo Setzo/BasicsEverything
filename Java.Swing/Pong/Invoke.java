be75812c318d1e7040c1e9845705b93b7f03d3a6
Wojtek Pruszak <setzo94@gmail.com>
2015-03-10 16:51:12 +0100

    Create Invoke.java

---
+++ Java.Swing/Pong/Invoke.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.SwingUtilities;
+
+public class Invoke {
+
+	public static void main(String[] args) {
+
+			SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+
+					new MainFrame();
+				}
+			});
+	}
+}
