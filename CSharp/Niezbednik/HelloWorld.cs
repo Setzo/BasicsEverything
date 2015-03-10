6aee85ac362363d187628fce80af220008445186
Wojtek Pruszak <setzo94@gmail.com>
2015-03-10 17:38:53 +0100

    Create HelloWorld.cs

---
+++ CSharp/Niezbednik/HelloWorld.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleeee
+{
+	public class HelloWorld
+	{
+		public void Hello()
+		{
+			Console.WriteLine("Hello");
+		}
+	}
+}
