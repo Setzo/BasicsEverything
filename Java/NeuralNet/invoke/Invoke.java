732a274f6777aa031831ac02a077757dff9f658b
Wojtek Pruszak <setzo94@gmail.com>
2015-03-12 17:23:23 +0100

    Create Invoke.java

---
+++ Java/NeuralNet/invoke/Invoke.java
@@ -0,0 +1,49 @@
+package invoke;
+import java.util.Vector;
+
+import neural.Net;
+import training.TrainingData;
+
+
+public class Invoke {
+
+	public static void main(String[] args) {
+
+		TrainingData trainingData = new TrainingData("C:\\Users\\Setzo\\Desktop\\tmp.txt");
+		
+		Vector<Integer> topology = trainingData.getTopology();
+		
+		Net myNet = new Net(topology);
+		
+		int trainingPass = 0;
+		
+		while(!trainingData.isEof()) {
+			
+			System.out.printf("\nPass #%d\n", ++trainingPass);
+			
+			Vector<Double> input = trainingData.getInput();
+			
+			if(topology.get(0) != input.size()) {
+				break;
+			}
+			
+			System.out.println("Input: " + input.toString());
+			myNet.feedForward(input);
+			
+			Vector<Double> result = myNet.getResultValues();
+			System.out.print("Result: [");
+			for(int i = 0; i < result.size(); i++) {
+				System.out.printf("%1.6f", result.get(i));
+				if(i + 1 != result.size()) {
+					System.out.print(" ");
+				}
+			}
+			System.out.print("]\n");
+			
+			Vector<Double> desiredOutput = trainingData.getOutput();
+			System.out.println("Target: " + desiredOutput.toString());
+			
+			myNet.backPropagation(desiredOutput);
+		}
+	}
+}
