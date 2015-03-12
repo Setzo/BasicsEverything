ce379db45b1c1b3d81df476cca3a6b23bd08c6fa
Wojtek Pruszak <setzo94@gmail.com>
2015-03-12 17:24:10 +0100

    Create Layer.java

---
+++ Java/NeuralNet/neural/Layer.java
@@ -0,0 +1,12 @@
+package neural;
+import java.util.Vector;
+
+
+public class Layer extends Vector<Neuron>{
+	
+	private static final long serialVersionUID = -8999151764582596668L;
+
+	public Layer() {
+		super();
+	}
+}
