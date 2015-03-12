79b9721bead60e6b81438195ef707535ce47eb9d
Wojtek Pruszak <setzo94@gmail.com>
2015-03-12 18:34:01 +0100

    Update Test.java

--- Java/NeuralNet/test/Test.java
+++ Java/NeuralNet/test/Test.java
@@ -5,7 +5,7 @@ import training.CreateSampleData;
 public class Test {
 	public static void main(String[] args) {
 		
-		String t = "1 2 3 7";
-		CreateSampleData test = new CreateSampleData("file.txt", CreateSampleData.add, t);
+		String t = "2 3 1";
+		CreateSampleData test = new CreateSampleData("file.txt", CreateSampleData.xor, t);
 	}
 }
