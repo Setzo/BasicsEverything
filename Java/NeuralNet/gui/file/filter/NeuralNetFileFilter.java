ec3eeb078f3a734affb4b3bb690a9668b1740f95
Wojtek Pruszak <setzo94@gmail.com>
2015-03-14 16:16:22 +0100

    Create NeuralNetFileFilter.java

---
+++ Java/NeuralNet/gui/file/filter/NeuralNetFileFilter.java
@@ -0,0 +1,28 @@
+package gui.file.filter;
+
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+
+import uti.Utilitiess;
+
+public class NeuralNetFileFilter extends FileFilter {
+
+	public boolean accept(File file) {
+		
+		if(file.isDirectory()) {
+			return true;
+		}
+		
+		if(Utilitiess.getExtension(file.getName()) == "nnet") {
+			return true;
+		}
+		
+		return false;
+	}
+
+	public String getDescription() {
+		
+		return "Neural network training files (.nnet)";
+	}
+}
