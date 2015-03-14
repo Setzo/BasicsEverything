76b07bb0d64db341a0218c3503182917c3284224
Wojtek Pruszak <setzo94@gmail.com>
2015-03-14 16:18:47 +0100

    Update Invoke.java

--- Java/NeuralNet/invoke/Invoke.java
+++ Java/NeuralNet/invoke/Invoke.java
@@ -1,55 +1,16 @@
 package invoke;
-import java.util.Vector;
 
-import neural.Net;
-import training.CreateSampleData;
-import training.TrainingData;
+import javax.swing.SwingUtilities;
 
+import gui.main.MainFrame;
 
 public class Invoke {
 
 	public static void main(String[] args) {
 		
-		CreateSampleData csd = new CreateSampleData("adding.txt"
-				, CreateSampleData.xor
-				, "2 3 1"
-				, 5000);
-		
-		TrainingData trainingData = new TrainingData(csd.getSampleFile());
-		
-		Vector<Integer> topology = trainingData.getTopology();
-		
-		Net myNet = new Net(topology);
-		
-		int trainingPass = 0;
-		
-		while(!trainingData.isEof()) {
+		SwingUtilities.invokeLater(() -> {
 			
-			System.out.printf("\nPass #%d\n", ++trainingPass);
-			
-			Vector<Double> input = trainingData.getInput();
-			
-			if(topology.get(0) != input.size()) {
-				break;
-			}
-			
-			System.out.println("Input: " + input.toString());
-			myNet.feedForward(input);
-			
-			Vector<Double> result = myNet.getResultValues();
-			System.out.print("Result: [");
-			for(int i = 0; i < result.size(); i++) {
-				System.out.printf("%1.6f", result.get(i));
-				if(i + 1 != result.size()) {
-					System.out.print(" ");
-				}
-			}
-			System.out.print("]\n");
-			
-			Vector<Double> desiredOutput = trainingData.getOutput();
-			System.out.println("Target: " + desiredOutput.toString());
-			
-			myNet.backPropagation(desiredOutput);
-		}
+			MainFrame.getInstance("Neural Network Simulator alpha v0.0.1");
+		});
 	}
 }
