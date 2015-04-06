cbfe5a597513e1fac15bcaa606ce060975ae11da
Wojtek Pruszak <setzo94@gmail.com>
2015-04-06 17:02:55 +0200

    Update TestingFeatures.scala

--- Scala/TestingFeatures.scala
+++ Scala/TestingFeatures.scala
@@ -52,3 +52,16 @@ object TestingFeatures extends App {
 
 	rangeFilter
 }
+
+/*
+List(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+1, 4, 7, 10, 13, 
+List(1, 3, 5, 7, 9)
+Setzo
+ozteS
+Setzo
+SetzoSetzoSetzoSetzo
+500
+Vector(8, 9, 10)
+Vector(2, 4, 6, 8, 10, 12, 14, 16, 18, 20)
+*/
