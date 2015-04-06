84eb4ceeda4c49f5c8025367e40e4b40f84a9232
Wojtek Pruszak <setzo94@gmail.com>
2015-04-06 15:40:22 +0200

    Create JavaToScala.java

---
+++ Scala/HelloWorld/JavaToScala.java
@@ -0,0 +1,12 @@
+package hello.world;
+
+public class JavaToScala {
+	
+	public static void main(String[] args) {
+		
+		HelloWorldTrait trait = new ScalaTraitToJavaInterface();
+		
+		trait.sayHello();
+		trait.sayHelloMoreTimes();
+	}
+}
