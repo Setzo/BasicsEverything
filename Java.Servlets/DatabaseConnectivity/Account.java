43d7838b325846f973a241c863829db2a568df9f
Wojtek Pruszak <setzo94@gmail.com>
2015-04-09 15:52:58 +0200

    Create Account.java

---
+++ Java.Servlets/DatabaseConnectivity/Account.java
@@ -0,0 +1,41 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Account {
+
+	private Connection con;
+	
+	public Account(Connection con) {
+		this.con = con;
+	}
+	
+	public boolean login(String email, String password) throws SQLException {
+		
+		PreparedStatement prepSt =
+				con.prepareStatement("select count(*) as count from users where email=? and password=?");
+		
+		prepSt.setString(1, email);		//SQL - index od 1
+		prepSt.setString(2, password);
+		
+		ResultSet resSet = prepSt.executeQuery();
+		resSet.next();
+		
+		int cnt = 0;
+		
+		if(resSet.next()) {
+			cnt = resSet.getInt("count");
+		}
+		
+		resSet.close();
+		
+		if(cnt == 0) {
+			return false;
+		}
+		
+		return true;
+	}
+}
