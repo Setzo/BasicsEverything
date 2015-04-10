7285bc69640190a798ffcc143a96c8a639898d04
Wojtek Pruszak <setzo94@gmail.com>
2015-04-10 19:41:40 +0200

    Create App.java

---
+++ Java.Spring/HelloWorldEnterpriseEdition/App.java
@@ -0,0 +1,18 @@
+package tst;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class App {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+		
+		User user = (User)context.getBean("user");
+		
+		user.sayHello();
+	}
+
+}
