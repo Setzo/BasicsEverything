a5d82ffe2140c46c6dbb25c5e2659aab80c88606
Wojtek Pruszak <setzo94@gmail.com>
2015-04-10 15:18:33 +0200

    Create App.java

---
+++ Java.Servlets/MavenHelloWorld/App.java
@@ -0,0 +1,11 @@
+package setzo.basicseverything.test.testproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
