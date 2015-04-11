69d6bc2f794cbc36d5fc3cded12f41eb73b15528
Wojtek Pruszak <setzo94@gmail.com>
2015-04-11 15:15:57 +0200

    Create PassObjects1.java

---
+++ Java.Servlets/JSTL_Objects/PassObjects1.java
@@ -0,0 +1,49 @@
+package controller;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.User;
+
+public class PassObjects extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public PassObjects() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		User user1 = new User("User 1", 1);
+		User user2 = new User("User 2", 2);
+		User user3 = new User("User 3", 3);
+		
+		//REQUEST SCOPE BEAN
+		request.setAttribute("user1", user1);
+		
+		//SESSION SCOPE BEAN
+		request.getSession().setAttribute("user2", user2);
+		
+		//APPLICATION SCOPE BEAN
+		getServletContext().setAttribute("user3", user3);
+		
+		Map<String, String> map = new HashMap<String, String>();
+		
+		map.put("key", "value");
+		map.put("key0", "value0");
+		
+		request.setAttribute("map1", map);
+		
+		request.getRequestDispatcher("/receive-objects.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
