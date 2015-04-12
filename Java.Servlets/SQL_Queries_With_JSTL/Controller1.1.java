a74b3e6ff03bd1311eb5028d2fdd74c062b60527
Wojtek Pruszak <setzo94@gmail.com>
2015-04-12 15:34:22 +0200

    Create Controller1.1.java

---
+++ Java.Servlets/SQL_Queries_With_JSTL/Controller1.1.java
@@ -0,0 +1,41 @@
+package controllerr;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public Controller() {
+        super();
+    }
+
+	public void init(ServletConfig config) throws ServletException {
+		super.init(config);
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String action = request.getParameter("action");
+		Map<String, String> actionMap = new HashMap<String, String>();
+		actionMap.put("img", "/image.jsp");
+		actionMap.put("home", "/home.jsp");
+		
+		if(action == null || !actionMap.containsKey(action)) {
+			action = "home";
+		}
+		
+		request.getRequestDispatcher(actionMap.get(action)).forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	}
+
+}
