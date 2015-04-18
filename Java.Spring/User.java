ce37cc3a751340be809e339b4a901df9b7850103
Wojciech Pruszak <setzo94@gmail.com>
2015-04-18 20:30:30 +0200

    Create User.java

---
+++ Java.Spring/User.java
@@ -0,0 +1,47 @@
+package tst;
+
+public class User {
+
+	private int id;
+	private String login;
+	
+	private Address address;
+	
+	private int count;
+	
+	public User() {
+		
+	}
+	
+	public User(int id, String login) {
+
+		this.id = id;
+		this.login = login;
+	}
+
+	public void sayHello() {
+		System.out.println("Hello!");
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", login=" + login + ", address=" + address
+				+ ", count=" + count + "]";
+	}
+}
