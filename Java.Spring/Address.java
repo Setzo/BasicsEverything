57e1f32d6c28f90b018562ee13a86e9c5db2b4ff
Wojciech Pruszak <setzo94@gmail.com>
2015-04-18 20:30:42 +0200

    Create Address.java

---
+++ Java.Spring/Address.java
@@ -0,0 +1,30 @@
+package tst;
+
+public class Address {
+
+	private String address;
+	private String something;
+	
+	public Address(String address, String something) {
+		
+		this.address = address;
+		this.something = something;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getSomething() {
+		return something;
+	}
+	public void setSomething(String something) {
+		this.something = something;
+	}
+
+	public String toString() {
+		return "Address [address=" + address + ", something=" + something + "]";
+	}
+}
