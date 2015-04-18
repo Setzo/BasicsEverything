8bf618e31fd32374eb086c7b75c838401d81b628
Wojciech Pruszak <setzo94@gmail.com>
2015-04-18 19:22:12 +0200

    Create Main.java

---
+++ Java/FunctionParameter/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Functions fun = new Functions();
+		Tryout tryout = new Tryout();
+		
+		System.out.println(tryout.applyFunction(fun::countLength, "ddewrwer"));
+
+	}
+
+}
