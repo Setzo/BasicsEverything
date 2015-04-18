3958c6186842acc2aa095a661f6f66850cdfb3a3
Wojciech Pruszak <setzo94@gmail.com>
2015-04-18 20:30:20 +0200

    Create App.java

---
+++ Java.Spring/App.java
@@ -0,0 +1,25 @@
+package tst;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("tst/beans/beans.xml");
+		
+		User user = (User)context.getBean("user");
+		
+		user.sayHello();
+		
+		System.out.println(user);
+
+		Address adderss = (Address)context.getBean(tst.Address.class);
+		
+		System.out.println(adderss);
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
