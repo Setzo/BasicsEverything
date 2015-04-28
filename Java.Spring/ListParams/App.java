010d5d2910d02c963d9c4e00218a4a92c548c930
Wojciech Pruszak <setzo94@gmail.com>
2015-04-28 20:45:56 +0200

    Update App.java

--- Java.Spring/ListParams/App.java
+++ Java.Spring/ListParams/App.java
@@ -1,1 +1,22 @@
+package pack1;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import bucket.Bucket;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans/beans.xml");
+
+		Bucket bucket = (Bucket)context.getBean("bucket");
+		
+		System.out.println(bucket);
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
