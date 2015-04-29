0a425ef2dac2af08f0e8744cbbbb4b8519e6c554
Wojciech Pruszak <setzo94@gmail.com>
2015-04-29 22:27:27 +0200

    Create Jungl.java

---
+++ Java.Spring/InnerBean/Jungl.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+
+public class Jungl {
+	
+	private Map<String, String> map = new HashMap<String, String>();
+
+	public Map<String, String> getMap() {
+		return map;
+	}
+
+	public void setMap(Map<String, String> map) {
+		this.map = map;
+	}
+
+	@Override
+	public String toString() {
+		
+		StringBuilder sb = new StringBuilder();
+		
+		for(Map.Entry<String, String> entry : map.entrySet()) {
+			
+			sb.append(entry.getKey() + " : " + entry.getValue() + "\n");
+		}
+		
+		return sb.toString();
+	}
+}
