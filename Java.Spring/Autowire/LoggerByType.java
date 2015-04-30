467e62a87cf6351a1148ec6d3a01857dbc491a51
Wojciech Pruszak <setzo94@gmail.com>
2015-04-30 17:33:10 +0200

    Create LoggerByType.java

---
+++ Java.Spring/Autowire/LoggerByType.java
@@ -0,0 +1,24 @@
+package spring.testing;
+
+public class LoggerByType {
+
+	private ConsoleWriter consoleWriter;
+	private FileWriter fileWriter;
+
+	public void setConsoleWriter(ConsoleWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	public void setFileWriter(FileWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		consoleWriter.write(text);
+	}
+
+}
