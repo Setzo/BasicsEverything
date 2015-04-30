0692676a4adcf56522c781a3c024f4c1a1cbb03b
Wojciech Pruszak <setzo94@gmail.com>
2015-04-30 15:28:12 +0200

    Update Jungl.java

--- Java.Spring/InnerBean/Jungl.java
+++ Java.Spring/InnerBean/Jungl.java
@@ -1,11 +1,11 @@
 import java.util.HashMap;
 import java.util.Map;
 
-
-
 public class Jungl {
-	
+
 	private Map<String, String> map = new HashMap<String, String>();
+
+	private Map<String, Animal> amap = new HashMap<String, Animal>();
 
 	public Map<String, String> getMap() {
 		return map;
@@ -15,16 +15,29 @@ public class Jungl {
 		this.map = map;
 	}
 
+	public Map<String, Animal> getAmap() {
+		return amap;
+	}
+
+	public void setAmap(Map<String, Animal> amap) {
+		this.amap = amap;
+	}
+
 	@Override
 	public String toString() {
-		
+
 		StringBuilder sb = new StringBuilder();
-		
-		for(Map.Entry<String, String> entry : map.entrySet()) {
-			
+
+		for (Map.Entry<String, String> entry : map.entrySet()) {
+
 			sb.append(entry.getKey() + " : " + entry.getValue() + "\n");
 		}
-		
+
+		for (Map.Entry<String, Animal> entry : amap.entrySet()) {
+
+			sb.append(entry.getKey() + " : " + entry.getValue() + "\n");
+		}
+
 		return sb.toString();
 	}
 }
