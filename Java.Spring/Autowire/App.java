868f6a9919ede5cf04182a73362d34e8ed17f847
Wojciech Pruszak <setzo94@gmail.com>
2015-04-30 17:32:16 +0200

    Create App.java

---
+++ Java.Spring/Autowire/App.java
@@ -0,0 +1,31 @@
+package spring.testing;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"beans/beans.xml");
+
+		LoggerByType loggerbyType = (LoggerByType) context.getBean("loggerbytype");
+
+		loggerbyType.writeConsole("Hello there");
+		loggerbyType.writeFile("Hi again");
+
+		LoggerByName loggerByName = (LoggerByName) context.getBean("loggerbyname");
+
+		loggerByName.writeConsole("Hello there");
+		loggerByName.writeFile("Hi again");
+		
+		LoggerByConstructor loggerByConstructor = (LoggerByConstructor) context.getBean("loggerbyconstructor");
+
+		loggerByConstructor.writeConsole("Hello there");
+		loggerByConstructor.writeFile("Hi again");
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
+
+}
