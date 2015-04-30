e8f2c62f754e5a58fe609b2060cdd98703a98f72
Wojciech Pruszak <setzo94@gmail.com>
2015-04-30 17:32:27 +0200

    Create LoggerByName.java

---
+++ Java.Spring/Autowire/LoggerByName.java
@@ -0,0 +1,24 @@
+package spring.testing;
+
+public class LoggerByName {
+
+	private LogWriter consoleWriter;
+	private LogWriter fileWriter;
+
+	public void setConsoleWriter(LogWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		consoleWriter.write(text);
+	}
+
+}
