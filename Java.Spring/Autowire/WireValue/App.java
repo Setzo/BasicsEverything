992fd132e8201ae2c34b82dc8a723ca07043aa18
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 18:00:45 +0200

    Create App.java

---
+++ Java.Spring/Autowire/WireValue/App.java
@@ -0,0 +1,19 @@
+package main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans/beans.xml");
+		
+		ValueContainer vc = (ValueContainer)context.getBean("valueContainer");
+		
+		vc.type();
+		
+		((ClassPathXmlApplicationContext)context).close();
+	}
+
+}
