ee86d1d5f38e6e276a7460d3abd3a0baaffe91ed
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 17:44:43 +0200

    Create Logger.java

---
+++ Java.Spring/Autowire/AutowireAuto-Detect/Logger.java
@@ -0,0 +1,40 @@
+package spring.testing.annotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("logger")
+public class Logger {
+
+	@Autowired
+	private ConsoleWriter consoleWriter;
+	@Autowired
+	private FileWriter fileWriter;
+
+	/*@Autowired
+	public Logger(ConsoleWriter consoleWriter, FileWriter fileWriter) {
+		
+		this.consoleWriter = consoleWriter;
+		this.fileWriter = fileWriter;
+	}
+	
+	//By Type
+	// @Autowired
+	public void setConsoleWriter(ConsoleWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	// @Autowired
+	public void setFileWriter(FileWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}*/
+	
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		consoleWriter.write(text);
+	}
+
+}
