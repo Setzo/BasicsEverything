0b76a270dcc9e8f38bd1fdd3f321e7eb7a3095d2
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 14:23:54 +0200

    Create FileWriter.java

---
+++ Java.Spring/Autowire/AnnotationAutowire/FileWriter.java
@@ -0,0 +1,10 @@
+package spring.testing.annotations;
+
+public class FileWriter implements LogWriter {
+
+	public void write(String text) {
+		System.out.println("Write to file: " + text);
+		
+	}
+
+}
