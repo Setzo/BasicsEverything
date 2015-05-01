663c064037af6e4649ab1d4e4bf1517bbfc33e38
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 17:43:39 +0200

    Create App.java

---
+++ Java.Spring/Autowire/AutowireAuto-Detect/App.java
@@ -0,0 +1,36 @@
+package spring.testing.annotations;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"beans/annotations/beans.xml");
+
+		Logger logger = (Logger) context.getBean("logger");
+
+		logger.writeConsole("Hello there");
+		logger.writeFile("Hi again");
+		
+		LoggerOptional loggerOptional = (LoggerOptional) context.getBean("loggerOptional");
+
+		loggerOptional.writeConsole("Hello there");
+		loggerOptional.writeFile("Hi again");
+		
+		LoggerJSR loggerJSR = (LoggerJSR) context.getBean("loggerJSR");
+
+		loggerJSR.writeConsole("Hello there");
+		loggerJSR.writeFile("Hi again");
+		
+		LoggerInject loggerInject = (LoggerInject) context.getBean("loggerInject");
+
+		loggerInject.writeConsole("Hello there");
+		loggerInject.writeFile("Hi again");
+
+		((ClassPathXmlApplicationContext) context).close();
+	}
+
+}
