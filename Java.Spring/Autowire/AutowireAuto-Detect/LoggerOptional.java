9d23d3f18b15376d2d633a5f8b9c44c76a7a2cc0
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 17:44:34 +0200

    Create LoggerOptional.java

---
+++ Java.Spring/Autowire/AutowireAuto-Detect/LoggerOptional.java
@@ -0,0 +1,35 @@
+package spring.testing.annotations;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("loggerOptional")
+public class LoggerOptional {
+
+	private LogWriter consoleWriter;
+	private LogWriter fileWriter;
+	
+	@Autowired
+	@Qualifier("toConsole")
+	public void setConsoleWriter(LogWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	@Autowired
+	@Qualifier("toFile")
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		if(this.consoleWriter != null) {
+			consoleWriter.write(text);
+		}
+	}
+
+}
