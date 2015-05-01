80ac6103ccd74e8d70475f978a9b798abcd2aa5f
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 17:44:00 +0200

    Create FileWriter.java

---
+++ Java.Spring/Autowire/AutowireAuto-Detect/FileWriter.java
@@ -0,0 +1,15 @@
+package spring.testing.annotations;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component("fileWriter")
+@Qualifier("toFile")
+public class FileWriter implements LogWriter {
+
+	public void write(String text) {
+		System.out.println("Write to file: " + text);
+		
+	}
+
+}
