6b53965d2077b0dc716a080237ff2fa51b723a61
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 17:44:22 +0200

    Create LoggerJSR.java

---
+++ Java.Spring/Autowire/AutowireAuto-Detect/LoggerJSR.java
@@ -0,0 +1,44 @@
+package spring.testing.annotations;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Component;
+
+@Component("loggerJSR")
+public class LoggerJSR {
+
+	private LogWriter consoleWriter;
+	private LogWriter fileWriter;
+	
+	@Resource(name="consoleWriter")
+	public void setConsoleWriter(LogWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	@Resource(name="fileWriter")
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		if(this.consoleWriter != null) {
+			consoleWriter.write(text);
+		}
+	}
+	
+	@PostConstruct
+	public void init() {
+		System.out.println("init");
+	}
+
+	@PreDestroy
+	public void destroy() {
+		System.out.println("destroy");
+	}
+}
