50151aa3049d70f3e3bbd34627fbe506ae4bfb7a
Wojciech Pruszak <setzo94@gmail.com>
2015-05-01 17:44:11 +0200

    Create LoggerInject.java

---
+++ Java.Spring/Autowire/AutowireAuto-Detect/LoggerInject.java
@@ -0,0 +1,43 @@
+package spring.testing.annotations;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.springframework.stereotype.Component;
+
+@Component("loggerInject")
+public class LoggerInject {
+
+	private LogWriter consoleWriter;
+	private LogWriter fileWriter;
+	
+	@Inject
+	@Named(value="consoleWriter")
+	public void setConsoleWriter(LogWriter writer) {
+		this.consoleWriter = writer;
+	}
+
+	@Inject
+	@Named(value="fileWriter")
+	public void setFileWriter(LogWriter fileWriter) {
+		this.fileWriter = fileWriter;
+	}
+	
+	public void writeFile(String text) {
+		fileWriter.write(text);
+	}
+	
+	public void writeConsole(String text) {
+		if(this.consoleWriter != null) {
+			consoleWriter.write(text);
+		}
+	}
+	
+	public void init() {
+		System.out.println("init");
+	}
+
+	public void destroy() {
+		System.out.println("destroy");
+	}
+}
