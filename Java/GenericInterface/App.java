81d9e5c6205ea48302bcf50b7f5d48c12c87fee4
Wojciech Pruszak <setzo94@gmail.com>
2015-05-03 15:27:17 +0200

    Create App.java

---
+++ Java/GenericInterface/App.java
@@ -0,0 +1,17 @@
+package main;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Impl checkImplementation = new Impl();
+		
+		System.out.println(checkImplementation.eqls(5, 5));
+		
+		System.out.println(checkImplementation.eqls(2, 5));
+		
+		System.out.println(checkImplementation.eqls(5, 2));
+
+	}
+
+}
