a16f12f72d7e640990b3f0ea2a9aad296b4a1c0c
Wojciech Pruszak <setzo94@gmail.com>
2015-05-05 02:29:05 +0200

    Create LWJGL_HelloWorld.java

---
+++ Java.LWJGL/LWJGL_HelloWorld.java
@@ -0,0 +1,34 @@
+package gui;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Main {
+
+	public Main() {
+		
+		try {
+			
+			Display.setDisplayMode(new DisplayMode(640, 480));
+			Display.setTitle("Hello World");
+			Display.create();
+			
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		while(!Display.isCloseRequested()) {
+			
+			Display.update();
+			Display.sync(60);
+		}
+		
+		Display.destroy();
+	}
+	
+	public static void main(String[] args) {
+		
+		new Main();
+	}
+}
