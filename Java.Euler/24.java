39beb39e2285a4550627594dcb10a11bc93ad312
Wojciech Pruszak <setzo94@gmail.com>
2015-05-08 21:03:03 +0200

    Update 24.java

--- Java.Euler/24.java
+++ Java.Euler/24.java
@@ -4,7 +4,7 @@ public class Euler24 {
 
 	private static int[] digits = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	
-	public static void main(String[] args) throws InterruptedException{
+	public static void main(String[] args) {
 
 		for(int i = 0; i < 1E6 - 1; i++) {
 			permutate();
