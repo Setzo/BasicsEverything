030186fa6761e1edb08b4aeec4662fee4676a465
Wojciech Pruszak <setzo94@gmail.com>
2015-05-08 17:00:57 +0200

    Create OffersController.java

---
+++ Java.Spring/SpringMVCTest/OffersController.java
@@ -0,0 +1,13 @@
+package springify.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OffersController {
+
+	@RequestMapping("/")
+	public String showHome() {
+		return "home";
+	}
+}
