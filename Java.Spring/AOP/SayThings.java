eba080a266d0241f426b97544ea3181777e776f7
Wojciech Pruszak <setzo94@gmail.com>
2015-05-10 17:50:31 +0200

    Update SayThings.java

--- Java.Spring/AOP/SayThings.java
+++ Java.Spring/AOP/SayThings.java
@@ -8,4 +8,18 @@ public class SayThings {
 	public void sayHi() {
 		System.out.println("Hi.");
 	}
+	
+	public void sayHi(String hi) {
+		System.out.println(hi);
+	}
+	
+	public int sayHi(int hi) {
+		System.out.println(hi);
+		return hi;
+	}
+	
+	public Object doSth() {
+		System.out.println("Something");
+		return null;
+	}
 }
