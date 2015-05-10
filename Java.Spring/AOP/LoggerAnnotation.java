a736043785485893800b6ac3b6da7f0ebda13a92
Wojciech Pruszak <setzo94@gmail.com>
2015-05-10 17:50:12 +0200

    Update LoggerAnnotation.java

--- Java.Spring/AOP/LoggerAnnotation.java
+++ Java.Spring/AOP/LoggerAnnotation.java
@@ -15,12 +15,29 @@ public class Logger {
 		System.out.println("About to say Hi.");
 	}
 	
-	@After("sayHiPointcut()")
+	@After("sayHiRunEverytime()")
 	public void saidHi() {
 		System.out.println("Said hi.");
 	}
+	
+	@Before("sayHiStringPointcut()")
+	public void sayStrHi() {
+		System.out.println("Only once");
+	}
 
-	@Pointcut("execution(void main.SayThings.sayHi())")
+	@Pointcut("execution(* main.*.*(..))")
 	public void sayHiPointcut() {
+	}
+	
+	@Pointcut("execution(void main.SayThings.sayHi())")
+	public void sayHiNoArgsPointcut() {
+	}
+	
+	@Pointcut("execution(* *.*(..))")
+	public void sayHiRunEverytime() {
+	}
+	
+	@Pointcut("execution(void main.SayThings.sayHi(String))")
+	public void sayHiStringPointcut() {
 	}
 }
