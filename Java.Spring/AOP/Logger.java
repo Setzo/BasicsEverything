3bc0d18e28d1e20886d4c9a6869372e5e6f28c0b
Wojciech Pruszak <setzo94@gmail.com>
2015-05-10 13:35:27 +0200

    Create Logger.java

---
+++ Java.Spring/AOP/Logger.java
@@ -0,0 +1,11 @@
+package main;
+
+import org.springframework.stereotype.Component;
+
+@Component("logger")
+public class Logger {
+
+	public void aboutToSayHi() {
+		System.out.println("About to say Hi.");
+	}
+}
