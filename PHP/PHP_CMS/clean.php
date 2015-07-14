39cb43c8b63f133140d73e9e070b19c0e76b17ec
Wojciech Pruszak <setzo94@gmail.com>
2015-07-14 22:49:16 +0200

    Create clean.php

---
+++ PHP/PHP_CMS/clean.php
@@ -0,0 +1,21 @@
+<!DOCTYPE unspecified PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+
+<html>
+<head>
+	
+<?php
+	$title = "";
+	echo "<title>$title</title>";
+?>
+
+</head>
+<body>
+
+<?php
+	
+
+?>
+
+
+</body>
+</html>
