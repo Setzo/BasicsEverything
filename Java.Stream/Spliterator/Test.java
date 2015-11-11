0bb63017442e500d31e6efd85be24fd863ac214c
Wojciech Pruszak <setzo94@gmail.com>
2015-11-11 14:25:20 +0100

    Create Test.java

---
+++ Java.Stream/Spliterator/Test.java
@@ -0,0 +1,18 @@
+package maine;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.function.Function;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class Test {
+
+	public static void main(String[] args) throws IOException, URISyntaxException {
+		
+		(StreamSupport.stream(new PersonSpliterator(Stream.of(Files.lines(Paths.get(Test.class.getResource("plik.txt").toURI())), Files.lines(Paths.get(Test.class.getResource("plik2.txt").toURI()))).flatMap(Function.identity()).spliterator()), false)).forEach(System.out::println);
+	}
+	
+}
