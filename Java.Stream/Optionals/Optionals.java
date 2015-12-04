fb2c2ac6d22dd2eb4d8296361a595fca6b479665
Wojciech Pruszak <setzo94@gmail.com>
2015-12-04 18:04:33 +0100

    Optionals

---
+++ Java.Stream/Optionals/Optionals.java
@@ -0,0 +1,27 @@
+package opt;
+
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
+
+public class Optionals {
+
+	public static void main(String[] args) {
+
+		List<Integer> list = ThreadLocalRandom
+				.current()
+				.ints(100, 0, 1000)
+				.boxed()
+				.collect(Collectors.toList());
+		
+		System.out.println(list.size());
+		
+		OptionalDouble avg = list.stream()
+				.mapToInt(x -> x)
+				.average();
+		
+		System.out.println(avg.orElseGet(() -> 0));
+	}
+
+}
