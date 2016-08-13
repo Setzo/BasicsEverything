7fcb8e2fadaa20756007e88fd88dffd182841b75
Wojciech Pruszak <setzo94@gmail.com>
2016-08-13 13:31:35 +0200

    Question/answer models/basic controllers

---
+++ Ruby/Rails/FirstProject/app/controllers/answers_controller.rb
@@ -0,0 +1,15 @@
+class AnswersController < ApplicationController
+
+  def create
+    Answer.create(answer_params)
+
+    redirect_to root_path
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:email, :body, :question_id)
+  end
+
+end
