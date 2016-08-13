c8b9e8f70718ea1f0b63ccabd4176c42554afbfb
Wojciech Pruszak <setzo94@gmail.com>
2016-08-13 17:26:24 +0200

    Store email in session

--- Ruby/Rails/FirstProject/app/controllers/questions_controller.rb
+++ Ruby/Rails/FirstProject/app/controllers/questions_controller.rb
@@ -3,11 +3,14 @@ class QuestionsController < ApplicationController
   def create
     Question.create(question_params)
 
+    session[:current_user_email] = question_params[:email]
+
     redirect_to root_path
   end
 
   def show
     @question = Question.find(params[:id])
+    @answers = @question.answers.order(created_at: :desc)
   end
 
   private
