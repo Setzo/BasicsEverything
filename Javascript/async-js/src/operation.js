bb543ad5f64ff3e37c6eb9f03af566be2194e9fa
Wojciech Pruszak <setzo94@gmail.com>
2016-09-03 19:48:00 +0200

    Async js test.

---
+++ Javascript/async-js/src/operation.js
@@ -0,0 +1,59 @@
+const delayms = 1
+
+function getCurrentCity(callback) {
+  setTimeout(function () {
+
+    const city = "New York, NY"
+    callback(null, city)
+
+  }, delayms)
+}
+
+function getWeather(city, callback) {
+  setTimeout(function () {
+
+    if (!city) {
+      callback(new Error("City required to get weather"))
+      return
+    }
+
+    const weather = {
+      temp: 50
+    }
+
+    callback(null, weather)
+
+  }, delayms)
+}
+
+function getForecast(city, callback) {
+  setTimeout(function () {
+
+    if (!city) {
+      callback(new Error("City required to get forecast"))
+      return
+    }
+
+    const fiveDay = {
+      fiveDay: [60, 70, 80, 45, 50]
+    }
+
+    callback(null, fiveDay)
+
+  }, delayms)
+}
+
+function fetchCurrentCity(onError, onSuccess) {
+  getCurrentCity((error, result) => {
+    if(error) {
+      onError(error)
+      return
+    }
+    onSuccess(result)
+  })
+}
+
+test("fetchCurrentCity with separate success and error callbacks", _ => {
+
+  fetchCurrentCity(error => console.log(error), success => console.log(success))
+})
