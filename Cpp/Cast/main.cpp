6f9d855862709cb50e4cd6f62a420343aa55ae5f
Wojciech Pruszak <setzo94@gmail.com>
2016-09-24 19:07:58 +0200

    Enums in C++. Free functions.

--- Cpp/Cast/main.cpp
+++ Cpp/Cast/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "Person.h"
 #include "Developer.h"
+#include "status.h"
+#include "util.h"
+
+using std::cout;
+using std::endl;
 
 int main() {
 
     Person p0("Mike", "Ross", 231123);
     Developer d0("Wojciech", "Pruszak", 22, "Software Engineer");
 
-    std::cout << "Hello, World!" << std::endl;
+    cout << "Hello, World!" << endl;
+    Status s = Pending;
+    cout << s << endl;
+
+    cout << is_even(2) << endl;
+    cout << is_even(3) << endl;
     return 0;
 }
---
+++ Cpp/Cast/status.h
@@ -0,0 +1,5 @@
+enum Status {
+    Pending,
+    Approved,
+    Cancelled
+};
