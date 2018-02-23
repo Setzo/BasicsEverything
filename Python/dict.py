afa96c22defbbb74aad6b0ac928b391d1676630f
Wojciech Pruszak <Wojciech.Pruszak@dnvgl.com>
2018-02-23 18:40:34 +0100

    Dicts in python.

---
+++ Python/dict.py
@@ -0,0 +1,17 @@
+dictionary = {
+    "key1": "somethgin",
+    'key2': 'sperieofejr',
+    'iejfw': 'fewoihfw'
+}
+
+print(dictionary)
+del dictionary['key1']
+print(dictionary)
+dictionary['key2'] = 2
+print(dictionary)
+
+# Error
+# print(dictionary['non existing key'])
+
+# No error
+print(dictionary.get('non existing key'))
