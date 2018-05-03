a179dd4f4a9f391284507b8cfd44dc28fac9ac8f
Wojciech Pruszak <info@wpruszak.com>
2018-05-03 15:11:24 +0200

    Callable test in Python.

---
+++ Python/callables.py
@@ -0,0 +1,38 @@
+import socket
+
+
+class Resolver:
+    def __init__(self):
+        self._cache = {}
+
+    def __call__(self, host):
+        if host not in self._cache:
+            self._cache[host] = socket.gethostbyname(host)
+        return self._cache[host]
+
+    def clear(self):
+        self._cache.clear()
+
+    def has_host(self, host):
+        return host in self._cache
+
+
+resolver = Resolver()
+
+print(resolver('amazon.com'))
+
+print(resolver._cache)
+print(resolver.has_host('amazon.com'))
+resolver.clear()
+print(resolver.has_host('amazon.com'))
+
+
+def sequence_class(is_immutable=False):
+    return tuple if is_immutable else list
+
+
+print(sequence_class())
+print(sequence_class(True))
+print(sequence_class(False))
+print(sequence_class(True)([1, 2, 3]))
+print(sequence_class(False)([1, 2, 3]))
